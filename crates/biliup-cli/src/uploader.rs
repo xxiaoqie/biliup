83e90e3805708399ea204c3c208729c7bae122cf
企鹅君 <47202121+xxiaoqie@users.noreply.github.com>
2026-05-29 16:59:54 +0800

    print upload

--- crates/biliup-cli/src/uploader.rs
+++ crates/biliup-cli/src/uploader.rs
@@ -562,7 +562,7 @@ pub async fn upload(
             t as f64 / 1000.,
             total_size as f64 / 1000. / t as f64
         );
-        info!("Bucket: {:?}", uploader.line);
+        info!("uploader: {:?}", uploader);
 
         // 保存断点续传信息
         checkpoint.add_video(video_path, video.clone());
